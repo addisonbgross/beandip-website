959841878a41b546db5d1b47869bd5ee363a358f
Addison <addisonbgross@gmail.com>
2022-05-25 17:01:14 -0400

    add prettier, eslint, and jss

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,34 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+import { createUseStyles } from 'react-jss';
+import { SheetsRegistry, JssProvider, createGenerateId } from 'react-jss';
+
+const useStyles = createUseStyles({
+  container: {
+    padding: '0 2rem',
+    backgroundColor: '#EEE',
+  },
+});
 
 export default function Home() {
+  const classes = useStyles();
+  const registry = new SheetsRegistry();
+  const generateId = createGenerateId();
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>beandip games</title>
-        <meta name="description" content="beandip games" />
-        <link rel="icon" href="/bean.ico" />
-      </Head>
+    <JssProvider registry={registry} generateId={generateId}>
+      <div className={classes.container}>
+        <Head>
+          <title>beandip games</title>
+          <meta name="description" content="beandip games" />
+          <link rel="icon" href="/bean.ico" />
+        </Head>
 
-      <main className={styles.main}>
-
-        <p className={styles.description}>
-          beandip
-        </p>
-
-      </main>
-
-      <footer className={styles.footer}>
-
-      </footer>
-    </div>
-  )
+        <main className={styles.main}>
+          <p className={styles.description}>beandip</p>
+        </main>
+      </div>
+    </JssProvider>
+  );
 }
