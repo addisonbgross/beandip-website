253d8b63e22286618f7ab6fc4052d65643d12e60
Addison <addisonbgross@gmail.com>
2022-10-05 21:42:03 -0600

    feat: october 5th blog post

--- components/about/about.js
+++ components/about/about.js
@@ -5,10 +5,10 @@ import styles from './about.module.css';
 const About = () => (
   <div>
     <p className={styles.summary}>
-      <strong>beandip games</strong> is a Canadian indie game currently studio
-      currently working on its first game. We aim to create unique (and slightly
-      weird) gaming experiences. Follow along with our development blog as we
-      share our creation process in a holistic way.
+      <strong>beandip games</strong> is a Canadian indie game studio currently
+      working on its first game. We aim to create unique (and slightly weird)
+      gaming experiences. Follow along with our development blog as we share our
+      creation process in a holistic way.
     </p>
 
     <div className={styles.employees}>
