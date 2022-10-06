60bad480256408f3a15b89d0d0da206b4e4ec35d
Addison <addisonbgross@gmail.com>
2022-10-06 13:31:58 -0600

    feat: add twitter card info

--- pages/_app.js
+++ pages/_app.js
@@ -18,6 +18,18 @@ const App = ({ Component, pageProps }) => (
         href="https://fonts.bunny.net/css?family=be-vietnam-pro:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i|sora:100,200,300,400,500,600,700,800"
         rel="stylesheet"
       />
+
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="@beandipgames" />
+      <meta name="twitter:title" content="beandip games dev blog" />
+      <meta
+        name="twitter:description"
+        content="Follow beandip games as they create their first game"
+      />
+      <meta
+        name="twitter:image"
+        content="https://beandipgames.com/images/hill-forest.png"
+      />
     </Head>
     <Component {...pageProps} />
   </>
